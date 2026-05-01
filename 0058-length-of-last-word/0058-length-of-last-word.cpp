aae95f527bb25375156233f257f211f03d212aac
rupamkumari4567 <rupamkumari03619@gmail.com>
2026-05-01 20:37:58 +0530

    Time: 0 ms (100%), Space: 8.8 MB (67.11%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int lengthOfLastWord(string s) {
        int n=s.length();
@@ -14,5 +14,15 @@ public:
        return length;
 
         
+    }
+};*/
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int idx=s.find_last_not_of(' ')+1;
+        s.erase(idx);
+       int last_space_idx=s.find_last_of(' ');
+        return last_space_idx==string::npos? s.length():s.length()-last_space_idx-1;
     }
 };
