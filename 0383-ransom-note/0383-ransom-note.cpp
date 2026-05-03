5aa3f6c6f91935eb5e27b8b77f501d01aa80ea57
rupamkumari4567 <rupamkumari03619@gmail.com>
2026-05-03 10:34:44 +0530

    Time: 11 ms (10.19%), Space: 11.7 MB (88.69%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        /*int n=ransomNote.length();
-        int m=magazine.length();
-        for(int i=0; i<n; i++)
-            for(int j=0; j<m; j++)
-                if(n[i]!=m[j]){
-                return false;
-            }
-            else if(n[i]==m[j]){
-                return true;
-
-            }
-            else if(n[i+1]==m[j]){
-                return true;
-            }
-        
-       return true; */
-
-
-
        sort(ransomNote.begin(), ransomNote.end());
        sort(magazine.begin(), magazine.end());
        int i=0;
