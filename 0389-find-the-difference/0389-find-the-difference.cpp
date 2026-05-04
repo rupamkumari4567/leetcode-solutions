5c222c3743be759a503a84cc80343eedf6907579
rupamkumari4567 <rupamkumari03619@gmail.com>
2026-05-04 14:15:14 +0530

    Time: 0 ms (100%), Space: 9.1 MB (76.24%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int m=0;
+       int m=0;
         for(char c:s)m^=c;
         for(char c:t)m^=c;
         return m;
         
     }
 };
+
